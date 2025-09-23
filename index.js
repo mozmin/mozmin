bbae3eba8174d288f92fd7fdd85bb7165513493b
mojiri00 <moguricamera@gmail.com>
2025-09-23 14:23:38 +0900

    index.js 수정

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let text = `
 
 ## Contact
 
-[![jmmo0722@naver.com](https://img.shields.io/badge/Gmail-D14836.svg?&logo=gmail&logoColor=white)](mailto:jmmo0722@naver.com)
+[![jmmo0722@naver.com](https://img.shields.io/badge/jmmo0722@naver.com-D14836.svg?&logo=gmail&logoColor=03C75A)](mailto:jmmo0722@naver.com)
 [![LinkedIn](https://img.shields.io/badge/LinkedIn-0A66C2?&logo=linkedin&logoColor=fff)](https://www.linkedin.com/in/%EC%A0%95%EB%AF%BC-%EB%AA%A8-289504385/)
 [![tistory](https://img.shields.io/badge/tistory-E34F26.svg?&logo=tistory&logoColor=white)](https://mozmin.tistory.com)
 
